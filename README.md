23f3d4f883811380305a0d56fba229a0be502b14
Anthony Liu <anthonyscottliu@gmail.com>
2018-05-11 18:26:50 -0400

    Proof of concept tool.

---
+++ README.md
@@ -0,0 +1,12 @@
+IRL Matcher
+==
+
+To foster a sense of community within TechX, we want to have a system of random lunch matches so that people across the organization can get to know one another. People have tough availability constraints and preferences over the kinds of people they want to meet, so assigning lunch dates is a challenging optimization problem. This tool helps a human perform that task.
+
+## Development
+
+This tool is totally client side. Run it with `python -m SimpleHTTPServer` or something.
+
+## License
+
+MIT License
--- js/main.js
+++ js/main.js
@@ -3,8 +3,55 @@ const IRLMatcher = (() => {
     document.getElementById('load').addEventListener('click', () => {
       const text = document.getElementById('data').value;
       const parsed = parseData(text);
-      console.log(parsed);
+      const grouped = groupByAvailability(parsed);
+      render(grouped);
     });
+  }
+
+  function getIdFromName(name) {
+    return name.toLowerCase().replace(/ /g, '-');
+  }
+
+  function render(groups) {
+    const container = document.getElementById('container');
+    for (let date in groups) {
+      const row = document.createElement('div');
+      const h3 = document.createElement('h3');
+      h3.innerHTML = date;
+      const ul = document.createElement('ul');
+      groups[date].forEach(person => {
+        const li = document.createElement('li');
+        const id = getIdFromName(person);
+        li.style.cursor = 'pointer';
+        li.className = id;
+        li.innerHTML = person;
+        li.addEventListener('click', () => {
+          Array.from(document.getElementsByClassName(id)).forEach(el => {
+            if (el.className.indexOf(' ') !== -1) {
+              el.className = el.className.split(' ')[0];
+            } else {
+              el.className = el.className + ' gray';
+            }
+          });
+        });
+        ul.appendChild(li);
+      });
+      row.appendChild(h3);
+      row.appendChild(ul);
+      container.appendChild(row);
+    }
+  }
+
+  function groupByAvailability(parsed) {
+    const slots = {};
+    parsed.forEach(r => {
+      r.availability.forEach(date => {
+        if (!slots.hasOwnProperty(date)) slots[date] = [];
+
+        slots[date].push(r.name);
+      });
+    });
+    return slots;
   }
 
   function parseData(data) {
@@ -12,7 +59,11 @@ const IRLMatcher = (() => {
   }
 
   function parseRecord(record) {
-    return record.split('\t');
+    const components = record.split('\t');
+    return {
+      name: components[1],
+      availability: components[3].split(',')
+    };
   }
 
   return {
