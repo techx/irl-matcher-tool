56377006927aa283111efcd9284fec91995106a8
Anthony Liu <anthonyscottliu@gmail.com>
2018-05-11 19:10:41 -0400

    Super ghetto by-day sorting.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const IRLMatcher = (() => {
       const text = document.getElementById('data').value;
       const parsed = parseData(text);
       const grouped = groupByAvailability(parsed);
-      render(grouped);
+      const days = groupByDay(grouped);
+      render(days);
     });
   }
 
@@ -12,35 +13,59 @@ const IRLMatcher = (() => {
     return name.toLowerCase().replace(/ /g, '-');
   }
 
-  function render(groups) {
+  function render(days) {
     const container = document.getElementById('container');
-    for (let date in groups) {
-      const row = document.createElement('div');
-      const h3 = document.createElement('h3');
-      h3.innerHTML = date;
-      const ul = document.createElement('ul');
-      groups[date].forEach(person => {
-        const li = document.createElement('li');
-        const id = getIdFromName(person);
-        li.style.cursor = 'pointer';
-        li.className = id;
-        li.innerHTML = person;
-        li.addEventListener('click', () => {
-          Array.from(document.getElementsByClassName(id)).forEach(el => {
-            if (el.className.indexOf(' ') !== -1) {
-              el.className = el.className.split(' ')[0];
-            } else {
-              el.className = el.className + ' gray';
-            }
+    const colors = [
+      '#ffdddd', '#ddffdd', '#ddddff',
+      '#ddffff', '#ffddff', '#ffffdd',
+      '#eeeeee'
+    ];
+    let i = 0;
+    console.log(days);
+    for (let day in days) {
+      const dayDiv = document.createElement('div');
+      dayDiv.style.background = colors[i++];
+      dayDiv.className = 'day';
+      for (let date in days[day]) {
+        const row = document.createElement('div');
+        const h3 = document.createElement('h3');
+        h3.innerHTML = date;
+        const ul = document.createElement('ul');
+        days[day][date].forEach(person => {
+          const li = document.createElement('li');
+          const id = getIdFromName(person);
+          li.style.cursor = 'pointer';
+          li.className = id;
+          li.innerHTML = person;
+          li.addEventListener('click', () => {
+            Array.from(document.getElementsByClassName(id)).forEach(el => {
+              if (el.className.indexOf(' ') !== -1) {
+                el.className = el.className.split(' ')[0];
+              } else {
+                el.className = el.className + ' gray';
+              }
+            });
           });
+          ul.appendChild(li);
         });
-        ul.appendChild(li);
-      });
-      row.appendChild(h3);
-      row.appendChild(ul);
-      container.appendChild(row);
+        row.appendChild(h3);
+        row.appendChild(ul);
+        dayDiv.appendChild(row);
+      }
+      container.appendChild(dayDiv)
     }
   }
+
+  function groupByDay(data) {
+    const days = {};
+    for (let date in data) {
+      const day = date.split(' ').slice(0, 2).join(' ');
+      if (!days.hasOwnProperty(day)) days[day] = {};
+      days[day][date] = data[date];
+    }
+    return days;
+  }
+
 
   function groupByAvailability(parsed) {
     const slots = {};
@@ -55,14 +80,14 @@ const IRLMatcher = (() => {
   }
 
   function parseData(data) {
-    return data.split('\n').map(r => parseRecord(r));
+    return data.split('\n').slice(1).map(r => parseRecord(r.trim()));
   }
 
   function parseRecord(record) {
     const components = record.split('\t');
     return {
       name: components[1],
-      availability: components[3].split(',')
+      availability: components[3].split(',').map(a => a.trim())
     };
   }
 
